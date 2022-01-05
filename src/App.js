4e92f318a65173e4a7e3c2c9b0c3d50f017bdf88
bolotbekMM <bolotbek.muratov@gmail.com>
2022-01-05 11:21:56 +0600

    css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import classes from "./App.module.css";
+import Header from "./components/header/Header";
+import Time from "./components/timer/Time";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app} >
+      <Header/>
+      <Time/>
+      
     </div>
   );
 }
